fc9996d7f98c0db0c416bbecc2853b1e0264f0a4
Maximilien LAVERNE <laverne.maximilien@yahoo.fr>
2020-03-24 13:19:35 +0100

    style home view

--- View/homeView.php
+++ View/homeView.php
@@ -5,6 +5,16 @@ ob_start();
 
 ?>
 <main>
+	<div id="hero">
+		<h2>BILLET SIMPLE POUR L'ALASKA</h2>
+		<div class="button"><a href="index.php?action=chaptersList">Commencer la lecture</a></div>
+	</div>
+	<div id="welcome">
+		<p>Bienvenue sur mon blog dédié à mon nouveau roman <span>Billet simple pour l'Alaska</span>.</p>
+		<p>Les chapitres seront publiés les uns aprés les autres en fonction de mon inspiration.</p>
+		<p>Vous pouvez participer au développement de mon roman en commentant mes publications, pour celà je vous invite à creer un compte.</p>
+		<p>Je vous souhaite une bonne lecture !</p>
+	</div>
 </main>
 
 <footer>
